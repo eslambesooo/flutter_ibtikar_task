f15ff13bc0e51d606e6201206a6042b742d700f8
Eslam Mohamed <imb@saed.sa>
2022-06-22 00:18:41 +0200

    init network_helper.dart for handle network requests

---
+++ lib/app/network/network_helper.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:get/get_connect/connect.dart';
+
+import '../utils/constants.dart';
+
+class NetworkHelper extends GetConnect {
+  // Get request
+  Future<Response> getPopularList(int page) => get('$kBaseUrl/person/popular?api_key=$kApiKey&language=${Get.deviceLocale!.languageCode.toLowerCase()}&page=$page');
+  Future<Response> getDetails(String personId) => get('$kBaseUrl/person/$personId?api_key=$kApiKey&language=${Get.deviceLocale!.languageCode.toLowerCase()}');
+  Future<Response> getPersonImages(String personId) => get('$kBaseUrl/person/$personId/images?api_key=$kApiKey&language=${Get.deviceLocale!.languageCode.toLowerCase()}');
+}
---
+++ lib/app/utils/constants.dart
@@ -0,0 +1,2 @@
+const String kBaseUrl = "https://api.themoviedb.org/3/";
+const String kApiKey = "f2510ee9fba3eb688842e6a2e12eb3da";
